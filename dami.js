6eaebf77b469eca680a81e4127f6c97f6cdcf130
alumno <alumno@Instituto.local>
2023-08-04 08:56:31 -0300

    javascript
    tp de javascript

--- dami.js
+++ dami.js
@@ -1,1 +1,12 @@
-r
+console.log (number = 1000 + 5000)
+console.log (number = 500 - 50)
+console.log (number = 400 * 700)
+console.log (number = 1000 / 2)
+alert (30 + 50)
+alert (35 - 5)
+alert (600 * 5000)
+alert (4 / 8)
+let nombreVariable = 'Mi edad es'
+alert ('Mi edad es') = (2023 - 2009)
+
+
